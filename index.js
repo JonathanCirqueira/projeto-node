635671969bab195dd7a595caf68c78f608eb2918
Jonathan <jonathancirqueira96@gmail.com>
2022-12-05 22:13:47 -0300

    First-Node-Project

---
+++ index.js
@@ -0,0 +1,79 @@
+
+const { request, response } = require('express')
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+
+const users = []
+
+const checkUserid = (request, response, next) => {
+
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ mensagem: "User not found" })
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+
+}
+
+
+
+app.get('/users', (request, response) => {
+    return response.json(users)
+
+})
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+
+})
+
+app.put('/users/:id',checkUserid, (request, response) => {
+   
+    const { name, age } = request.body
+
+    const index = request.userIndex
+
+    const id = request.userId
+
+    const updateUser = { id, name, age }
+
+    
+    users[index] = updateUser
+
+
+    return response.json(updateUser)
+
+})
+app.delete('/users/:id',checkUserid, (request, response) => {
+
+    const index = request.userIndex
+
+
+
+    
+    users.splice(index, 1)
+
+    return response.status(204).json()
+
+})
+app.listen(port, () => {
+    console.log(`🚀server started on port ${port}`)
+})
